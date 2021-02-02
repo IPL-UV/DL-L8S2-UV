b475c21970d16cb6c15be8720d9e478b7c5bbb0f
dan <dan.lopez@uv.es>
2021-02-02 13:05:22 +0100

    Updated: dependencies, name and data include

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import dl_l8s2_uv
 
-setup(name='cmixuv',
-      version=0.1,
+setup(name='dl_l8s2_uv',
+      version=dl_l8s2_uv.__version__,
       description='Cloud masking of Landsat-8 and Sentinel-2 based on Deep Learning',
       author='Dan Lopez Puigdollers',
       author_email='dan.lopez@uv.es',
+      packages=find_packages(exclude=["tests"]),
+      package_data={'': ['*.hdf5']},
       include_package_data=True,
-      install_requires=["numpy", "lxml", "rasterio", "shapely", "spectral", "tensorflow", "luigi", "matplotlib"],
+      install_requires=["numpy", "lxml", "spectral", "luigi", "h5py"],
       zip_safe=False)
